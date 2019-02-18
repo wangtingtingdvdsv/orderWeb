f114bd3af76fd8e7a80ad76ccbc89c018b1b7b69
王婷婷 <2372454518@qq.com>
2019-02-18 16:36:55 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,6 @@ module.exports = {
     script: 'src/main.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
